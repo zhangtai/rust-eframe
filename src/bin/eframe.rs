0c3aa4f3b391c231f2cc49ce1c832a7a5eedad85
Tai Zhang <tai.zhang@outlook.com>
2022-11-30 11:42:09 +0800

    Add clap args

--- src/bin/eframe.rs
+++ src/bin/eframe.rs
@@ -2,10 +2,16 @@ use image::{DynamicImage, GenericImageView, RgbImage};
 use rust_it8951::{It8951, Inquiry, Mode, SystemInfo};
 use std::thread;
 use std::time::Duration;
-use std::env;
+use clap::Parser;
 
 struct DisplayDimention(u32, u32);
 struct DisplayShrinks(u32, u32);
+
+#[derive(Parser,Default,Debug)]
+struct Arguments {
+    file: String,
+    resize: String,
+}
 
 fn main() -> anyhow::Result<()> {
     println!("Start");
@@ -17,30 +23,31 @@ fn main() -> anyhow::Result<()> {
     print_display_info(inquiry, display_info);
 
     let dimention = DisplayDimention(display_info.width, display_info.height);
-    let shrinks = DisplayShrinks(64, 40);
+    let shrinks = DisplayShrinks(56, 34);
     let dwidth: u32 = display_info.width;
     let dheight: u32 = display_info.height;
+    let clear_display = true;
 
-    println!("Converting image");
-    let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
+    let args = Arguments::parse();
 
-    let img_from_file = image::open(file_path)?;
-    let rgb: RgbImage = RgbImage::new(dimention.0, dimention.1);
-    let cavas: DynamicImage = DynamicImage::ImageRgb8(rgb).grayscale();
+    if clear_display {
+        let rgb: RgbImage = RgbImage::new(dimention.0, dimention.1);
+        let cavas: DynamicImage = DynamicImage::ImageRgb8(rgb).grayscale();
+        it8951.update_region(&cavas, 0, 0, Mode::INIT)?;
+        thread::sleep(Duration::from_millis(1500));
+    }
 
     println!("Pushing image");
-    it8951.update_region(&cavas, 0, 0, Mode::INIT)?;
-    thread::sleep(Duration::from_millis(1500));
-    let img = convert_image(img_from_file, dimention, shrinks);
-    println!("FX: {}", img.width());
-    println!("FY: {}", img.height());
+    let img_from_file = image::open(args.file)?;
+    let img = convert_image(img_from_file, dimention, shrinks, args.resize);
 
-    let px: u32 = (dwidth-img.width()) / 2;
-    let py: u32 = (dheight-img.height()) / 2 - 14;
-
-    println!("PX: {}", px);
-    println!("PY: {}", py);
+    let px: u32 = (dwidth-img.width()) / 2 + 2;
+    let py: u32 = (dheight-img.height()) / 2 - 13;
+    
+    // println!("FX: {}", img.width());
+    // println!("FY: {}", img.height());
+    // println!("PX: {}", px);
+    // println!("PY: {}", py);
 
     it8951.update_region(&img, px, py, Mode::GC16)?;
     println!("End");
@@ -61,14 +68,24 @@ fn print_display_info(inquiry: Inquiry, display: &SystemInfo) {
     println!("    version: {}", display.version);
 }
 
-fn convert_image(input_image: DynamicImage, dimension: DisplayDimention, shrinks: DisplayShrinks) -> DynamicImage {
-    let flipped_grayscale: DynamicImage;
+fn convert_image(input_image: DynamicImage, dimension: DisplayDimention, shrinks: DisplayShrinks, resize: String) -> DynamicImage {
+    println!("Converting image");
+
+    let rotated: DynamicImage;
+    let resized: DynamicImage;
+
     if input_image.height() > input_image.width() {
         // input_image = input_image.rotate270();
-        flipped_grayscale = input_image.rotate270().fliph().grayscale();
+        rotated = input_image.rotate270().fliph().grayscale();
     } else {
-        flipped_grayscale = input_image.fliph().grayscale();
+        rotated = input_image.fliph().grayscale();
     }
-    let final_image = flipped_grayscale.resize(dimension.0 - shrinks.0, dimension.1 - shrinks.1, image::imageops::FilterType::Nearest);
-    return final_image;
+
+    if resize == "fill" {
+        resized = rotated.resize_to_fill(dimension.0 - shrinks.0, dimension.1 - shrinks.1, image::imageops::FilterType::Nearest);
+    } else {
+        resized = rotated.resize(dimension.0 - shrinks.0, dimension.1 - shrinks.1, image::imageops::FilterType::Nearest);
+    }
+
+    return resized;
 }
