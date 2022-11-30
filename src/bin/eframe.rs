681ac3bf5b1bb2e7c9f0e401492384aa94c5e44a
Tai Zhang <tai.zhang@outlook.com>
2022-11-30 10:32:42 +0800

    Add function convert_image

--- src/bin/eframe.rs
+++ src/bin/eframe.rs
@@ -1,55 +1,74 @@
 use image::{DynamicImage, GenericImageView, RgbImage};
-use rust_it8951::{It8951, Mode};
+use rust_it8951::{It8951, Inquiry, Mode, SystemInfo};
 use std::thread;
 use std::time::Duration;
 use std::env;
+
+struct DisplayDimention(u32, u32);
+struct DisplayShrinks(u32, u32);
 
 fn main() -> anyhow::Result<()> {
     println!("Start");
     let mut it8951 = It8951::connect()?;
 
-    let inquiry_result = it8951.inquiry()?;
-    println!("vendor: {}", inquiry_result.vendor);
-    println!("product: {}", inquiry_result.product);
-    println!("revision: {}", inquiry_result.revision);
+    let inquiry = it8951.inquiry()?;
     thread::sleep(Duration::from_millis(100));
-    println!("We are now reading data");
-    let display = it8951.get_system_info().unwrap();
-    println!("width: {}", display.width);
-    println!("height: {}", display.height);
-    println!("mode: {}", display.mode);
-    println!("version: {}", display.version);
+    let display_info = it8951.get_system_info().unwrap();
+    print_display_info(inquiry, display_info);
 
-    let dwidth: u32 = display.width;
-    let dheight: u32 = display.height;
-    let swidth: u32 = 60;  // Shrink W
-    let sheight: u32 = 40;  // Shrink H
+    let dimention = DisplayDimention(display_info.width, display_info.height);
+    let shrinks = DisplayShrinks(64, 40);
+    let dwidth: u32 = display_info.width;
+    let dheight: u32 = display_info.height;
 
-    println!("Display data");
+    println!("Converting image");
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    let rgb: RgbImage = RgbImage::new(dwidth, dheight);
-    let cavas: DynamicImage = DynamicImage::ImageRgb8(rgb).grayscale();
-    let mut img_from_file = image::open(file_path)?;
-    if img_from_file.height() > img_from_file.width() {
-        img_from_file = img_from_file.rotate270();
-    }
-    let flipped_grayscale = img_from_file.fliph().grayscale();
-    let final_image = flipped_grayscale.resize(dwidth - swidth, dheight - sheight, image::imageops::FilterType::Nearest);
 
+    let img_from_file = image::open(file_path)?;
+    let rgb: RgbImage = RgbImage::new(dimention.0, dimention.1);
+    let cavas: DynamicImage = DynamicImage::ImageRgb8(rgb).grayscale();
+
+    println!("Pushing image");
     it8951.update_region(&cavas, 0, 0, Mode::INIT)?;
     thread::sleep(Duration::from_millis(1500));
+    let img = convert_image(img_from_file, dimention, shrinks);
+    println!("FX: {}", img.width());
+    println!("FY: {}", img.height());
 
-    println!("FX: {}", final_image.width());
-    println!("FY: {}", final_image.height());
-
-    let px: u32 = (dwidth-final_image.width()) / 2;
-    let py: u32 = (dheight-final_image.height()) / 2 - 14;
+    let px: u32 = (dwidth-img.width()) / 2;
+    let py: u32 = (dheight-img.height()) / 2 - 14;
 
     println!("PX: {}", px);
     println!("PY: {}", py);
 
-    it8951.update_region(&final_image, px, py, Mode::GC16)?;
+    it8951.update_region(&img, px, py, Mode::GC16)?;
     println!("End");
     Ok(())
+}
+
+
+fn print_display_info(inquiry: Inquiry, display: &SystemInfo) {
+    println!("Reading device info:");
+
+    println!("    vendor: {}", inquiry.vendor);
+    println!("    product: {}", inquiry.product);
+    println!("    revision: {}", inquiry.revision);
+
+    println!("    width: {}", display.width);
+    println!("    height: {}", display.height);
+    println!("    mode: {}", display.mode);
+    println!("    version: {}", display.version);
+}
+
+fn convert_image(input_image: DynamicImage, dimension: DisplayDimention, shrinks: DisplayShrinks) -> DynamicImage {
+    let flipped_grayscale: DynamicImage;
+    if input_image.height() > input_image.width() {
+        // input_image = input_image.rotate270();
+        flipped_grayscale = input_image.rotate270().fliph().grayscale();
+    } else {
+        flipped_grayscale = input_image.fliph().grayscale();
+    }
+    let final_image = flipped_grayscale.resize(dimension.0 - shrinks.0, dimension.1 - shrinks.1, image::imageops::FilterType::Nearest);
+    return final_image;
 }
